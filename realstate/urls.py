8d67489cb074d26b1a03e96affd364bf748b1b8b
Marcle Rodrigues <maarclee@gmail.com>
2016-06-25 20:11:27 -0300

    Fix companies url name space

--- realstate/urls.py
+++ realstate/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^companies/', include("companies.urls", namespace="posts")),
+    url(r'^companies/', include("companies.urls", namespace="companies")),
 ]
